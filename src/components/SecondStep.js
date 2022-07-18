ea2bb7f289ecf179d372242e0b3ca4c13173e46f
Mikolaj Mielczarek <mielczarek.electro@gmail.com>
2022-07-18 18:44:28 +0200

    solution with useRef for second steep

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -1,25 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function SecondStep(props) {
 
     const url = "#";
-    const [name, setName] = useState("");
+    //useState version
+    /*const [name, setName] = useState("");
     const [lastName, setLastName] = useState("");
     const [dateBirth, setDateBirth] = useState("");
+    */
+    //useRef version - so right now 
+    //name, lastName and dateBirth are so similar to regular DOM object
+    //like querySelector :)
+    const name = useRef();
+    const lastName = useRef();
+    const dateBirth = useRef();
+    
     const [allow, setAllow] = useState("");
     
     //function for reset values of inputs
     const resetForm = () => {
-        setName("");
+        /*setName("");
         setLastName("");
         setDateBirth("");
+        */
+       name.current.value = "";
+       lastName.current.value = "";
     }
 
     //reset inputs and hide second step/show succes
     const handleSubmit = (e) => {
         e.preventDefault()
-        resetForm()
-        props.show(name, lastName, dateBirth, false)
+        props.show(name.current.value,
+             lastName.current.value,
+             dateBirth.current.value,
+             false)
+        resetForm()    
     }
 
     //check if date in input date is over eighteen
@@ -46,10 +61,11 @@ export default function SecondStep(props) {
                 <label>first name</label>
                 <div className="input-div">
                     <input type="text"
-                    onChange ={(e) => {
+                    /*onChange ={(e) => {
                         setName(e.target.value)
                     }}
-                    value={name}
+                    value={name}*/
+                    ref={name}
                     placeholder="e.g. Jessica"
                     required />
                 </div>
@@ -57,10 +73,11 @@ export default function SecondStep(props) {
                     <div className="input-div">
                     <input className="input"
                     type="text"
-                    onChange = {(e) => {
+                    /*onChange = {(e) => {
                         setLastName(e.target.value)
                     }}
-                    value={lastName}
+                    value={lastName}*/
+                    ref={lastName}
                     placeholder="e.g. Walton" 
                     required />
                 </div>
@@ -68,12 +85,17 @@ export default function SecondStep(props) {
                     <div className="input-div">
                     <input className="input"
                     type="date"
-                    onChange = {(e) => {
+                    /*onChange = {(e) => {
                         setAllow(true)
                         setDateBirth(e.target.value)
                         overEighteen(e.target.value)
                     }}
-                    value={dateBirth}
+                    value={dateBirth}*/
+                    onChange = {(e) => {
+                        setAllow(true)
+                        overEighteen(dateBirth.current.value)
+                    }}
+                    ref={dateBirth}
                     required />
                 </div>
                 <h2 className="password-requirements">You should be minium 18 years old</h2>
